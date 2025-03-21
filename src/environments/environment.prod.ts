0654cec73081dcbf487ab9859c1cf03c3d2dcf54
laaroussi <dali.laaroussi@gmail.com>
2025-03-21 09:50:16 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://aa8e42e8e382e468e84d1fd50ea3465c-699541066.eu-north-1.elb.amazonaws.com:8080/'
+  apiUrl: 'http://aa8e42e8e382e468e84d1fd50ea3465c-699541066.eu-north-1.elb.amazonaws.com:8080'
 };
