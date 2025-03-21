6fbd02602371a57389d7e029be427b8679047d4b
laaroussi <dali.laaroussi@gmail.com>
2025-03-21 08:15:12 +0100

    first commit

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,9 +1,21 @@
-(window as any).global = window;
-(window as any).process = {
-  version: '',
-  env: { DEBUG: undefined },
-};
-
-if (!window.crypto) {
-  console.warn("L'API Web Crypto n'est pas disponible dans ce navigateur.");
+if (!window.crypto || !window.crypto.subtle) {
+  console.warn("Web Crypto API is not available. Using a fallback.");
+  (window as any).crypto = {
+    subtle: {
+      digest: async (algorithm: string, data: ArrayBuffer) => {
+        const buffer = new Uint8Array(data);
+        let hash = "";
+        for (let i = 0; i < buffer.length; i++) {
+          hash += buffer[i].toString(16).padStart(2, "0");
+        }
+        return new TextEncoder().encode(hash);
+      }
+    },
+    getRandomValues: (array: Uint8Array): Uint8Array => {
+      for (let i = 0; i < array.length; i++) {
+        array[i] = Math.floor(Math.random() * 256);
+      }
+      return array;
+    }
+  };
 }
