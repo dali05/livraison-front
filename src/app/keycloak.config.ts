cf1102a73006640bf423afb7e2d86c8178f46e3f
laaroussi <dali.laaroussi@gmail.com>
2025-03-23 16:26:35 +0100

    first commit

--- src/app/keycloak.config.ts
+++ src/app/keycloak.config.ts
@@ -1,12 +1,13 @@
 
 import { KeycloakService } from 'keycloak-angular';
+import {environment} from '../environments/environment';
 
 export function initializeKeycloak(keycloak: KeycloakService): () => Promise<boolean> {
   return () =>
     keycloak.init({
       config: {
         url: 'https://keycloak-test-dev.duckdns.org/',
-        realm: 'livraison-back',
+        realm: environment.realm,
         clientId: 'livraison-front'
       },
       initOptions: {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://a7d04da12970242829c840527e25a786-391100802.eu-north-1.elb.amazonaws.com:8080'
+  apiUrl: 'https://a7d04da12970242829c840527e25a786-391100802.eu-north-1.elb.amazonaws.com:8080',
+  realm: 'livraison-back'
 };
